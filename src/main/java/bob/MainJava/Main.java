8c057ea62817fcafd72f21f9c451021f5cdf6385
Ylyas555 <ylyas_movlyamov@stu.indianhills.edu>
2024-11-21 12:27:27 -0600

    Change

---
+++ src/main/java/bob/MainJava/Main.java
@@ -0,0 +1,22 @@
+package bob.MainJava;
+
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        // Single - line comment in java
+
+        /*
+        * Multi - line comment in java
+        *
+        *
+        *
+        *
+        *  */
+        //JOptionPane.showMessageDialog(null, "This is a graphical Output");
+        System.out.println("Your random is: "  +  (1 + (int) (Math.random() * 10)));
+    }
+
+}
